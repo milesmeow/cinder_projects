453d05ad2d732e4a30b89bc4758e698751dbb43d
James Tu <tu.james@gmail.com>
2011-12-12 16:43:22 -0800

    Adding the basic Ball class files.

---
+++ CinderClass/Bouncy/include/Ball.h
@@ -0,0 +1,17 @@
+//
+//  Ball.h
+//  Bouncy
+//
+//  Created by James Tu on 12/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Bouncy_Ball_h
+#define Bouncy_Ball_h
+
+class Ball {
+public:
+	Ball();
+};
+
+#endif
---
+++ CinderClass/Bouncy/src/Ball.cpp
@@ -0,0 +1,13 @@
+//
+//  Ball.cpp
+//  Bouncy
+//
+//  Created by James Tu on 12/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "Ball.h"
+
+Ball::Ball() {
+    
+}
